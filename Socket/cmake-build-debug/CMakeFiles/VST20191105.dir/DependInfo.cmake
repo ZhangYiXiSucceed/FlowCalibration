c6a04e807ff4c0c9e559cdc9d87671a74bbe488b
ZhangYiXiSucceed <Guoqingzhang0813@163.com>
2019-11-27 15:21:16 +0800

    SocketServer

---
+++ Socket/cmake-build-debug/CMakeFiles/VST20191105.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vst_software/VST20191119/Mythread.c" "/home/vst_software/VST20191119/cmake-build-debug/CMakeFiles/VST20191105.dir/Mythread.c.o"
+  "/home/vst_software/VST20191119/Net.c" "/home/vst_software/VST20191119/cmake-build-debug/CMakeFiles/VST20191105.dir/Net.c.o"
+  "/home/vst_software/VST20191119/Queue.c" "/home/vst_software/VST20191119/cmake-build-debug/CMakeFiles/VST20191105.dir/Queue.c.o"
+  "/home/vst_software/VST20191119/ReadConfig.c" "/home/vst_software/VST20191119/cmake-build-debug/CMakeFiles/VST20191105.dir/ReadConfig.c.o"
+  "/home/vst_software/VST20191119/main.c" "/home/vst_software/VST20191119/cmake-build-debug/CMakeFiles/VST20191105.dir/main.c.o"
+  "/home/vst_software/VST20191119/timer.c" "/home/vst_software/VST20191119/cmake-build-debug/CMakeFiles/VST20191105.dir/timer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
